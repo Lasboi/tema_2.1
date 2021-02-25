f513fe58f14e8de87e9316155f17d05696195a31
LasBoi <lasboi1337@gmail.com>
2021-02-25 20:25:17 +0100

    Rettelse til cookies

--- include/cookies.php
+++ include/cookies.php
@@ -11,7 +11,7 @@
 echo '
 <div class="nk-cookie-banner alert alert-dark text-center mb-0" role="alert">
     &#x1F36A; Denne hjemmeside benytter sig af cookies for at forbedre din oplevelse. Du kan læse mere om dem ved at 
-    <a href="/privacy/" target="blank">trykke her</a>.<br>
+    <a href="#" target="blank">trykke her</a>.<br>
     <button type="button" class="btn btn-primary ml-3" onclick="window.nk_hideCookieBanner()">
         Det er forstået!
     </button>
