aa6c21b846bb42c251948ad124b29756ab60fd42
LasBoi <lasboi1337@gmail.com>
2021-03-01 13:34:37 +0100

    Oprettet template af nyhed 1

---
+++ include/news-1.php
@@ -0,0 +1,16 @@
+<!-- Inkludere bootstrap og styles styling -->
+<style>
+
+    <?php include "css/bootstrap.css";?>
+    <?php include "css/styles.css";?>
+
+</style>
+
+<?php
+
+echo '
+
+
+'
+
+?>
