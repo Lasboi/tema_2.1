6a57305583c40166557c0b05a60c50abee85162b
LasBoi <lasboi1337@gmail.com>
2021-03-01 14:11:24 +0100

    Opdateret vejrkortet så man kan skifte mellem forskellige kort

--- include/weather-map.php
+++ include/weather-map.php
@@ -23,22 +23,7 @@ echo '
 </div>
 </div>
 
-<script language="javascript">
-    function ShowTemp()
-    {
-        document.getElementById("image").src = "images/vejrekort.JPG";
-    }
-
-    function ShowAir()
-    {
-        document.getElementById("image").src = "images/vejrkort_air.PNG";
-    }
-    
-    function ShowWaves()
-    {
-        document.getElementById("image").src = "images/vejrkort_waves.PNG";
-    }
-</script>
+<script src="scripts/toogleMap.js"></script>
 '
 
 ?>
---
+++ scripts/toogleMap.js
@@ -0,0 +1,14 @@
+function ShowTemp()
+{
+    document.getElementById("image").src = "images/vejrekort.JPG";
+}
+
+function ShowAir()
+{
+    document.getElementById("image").src = "images/vejrkort_air.PNG";
+}
+
+function ShowWaves()
+{
+    document.getElementById("image").src = "images/vejrkort_waves.PNG";
+}
