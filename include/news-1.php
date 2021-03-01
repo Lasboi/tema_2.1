8eeeea6b2d7900e3b3797cd9148694e86a5c2dec
Isabella <Isab346c@edu.easj.dk>
2021-03-01 14:59:34 +0100

    Nyhed om marts måned, billede og tekst mm.

--- include/news-1.php
+++ include/news-1.php
@@ -8,9 +8,72 @@
 
 <?php
 
-echo '
+echo '<br>
+<div class="mainContent-content-wrap">
+<nav aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="index.php">Forside</a></li>
+    <li class="breadcrumb-item"><a href="nyheder.php">Nyheder</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Februar og vinteren 20/21 indtil nu</li>
+  </ol>
+</nav><br>
+<hr class="card-heading-line">
 
-
+<div class="varsel-overskrift">
+        <p class="fs-1">Februar og vinteren 20/21 indtil nu</p
+        <p class="fw-bold">Marts nærmer sig med hastige skridt, og kalendervinteren er ved at <br> 
+                                være slut. Med solidt vintervejr i frisk erindring, kan man fristes <br>
+                                til at tro, at vinteren har været kold i år, men spørger man <br>
+                                klimatologen, får man et lidt andet svar.</p></div>
+                                <br>
+<div class="Marts-info">
+  <div class="row">
+    <div class="col-sm">
+            <img src="images/ice.jpg" class="advarsel1" alt="advarsel1" width="1000" height="1000">
+            Oddesundbroen over Limfjorden. Foto: Leif K. Andersen.</div>
+    <div class="col-sm">
+     <p> DMI’s klimatologer har et vågent øje på, hvordan vind og vejr 
+     har været i Danmark, Færøerne og Grønland. Én af dem, Mikael Scharling, har netop 
+    set de seneste tal igennem for vinteren og februar indtil nu, og sætter 
+     vintervejret i perspektiv, for os der kun husker vejret én sæson tilbage.</p>
+      <br><br>
+      <p class="fw-bold">Februar er startet koldt. Men ligefrem usædvanlig koldt, eller bare almindeligt koldt?</p>
+      <br>
+      <p>Ifølge Mikael Scharling har de lave temperaturer den første halvdel af februar været til den usædvanlige side<br><br>
+                      ”Første halvdel af februar har været ganske kold indtil nu. Middeltemperatur de sidste 14 dage har for Danmark 
+                      ligget på -4,2 °C, og vi skal tilbage til januar 2013, for at finde noget tilsvarende. I december 2010 
+                      var det dog endnu koldere med -6,6 °C i gennemsnit over to uger. Et gennemsnit på -4,2 °C er langt 
+                      fra noget vi ser hvert år.”</p>
+       <br><br>
+    </div>
+  </div>
+</div>
+<div class="Marts-info2">
+        <p class="fw-bold">Ender februar 2021 kold?</p>
+      <br>
+      <p>Ser vi på klimanormalen, altså gennemsnittet over de sidste 30 år, ligger februar med en middeltemperatur på 1,5 °C. 
+                      Med en kraftig skelen til de seneste prognoser, der tyder på noget mildere vejr den sidste halvdel af februar, 
+                      har Mikael Scharling et kvalificeret bud på, hvordan februar ender:
+                      <br><br>
+                      ”Et forsigtigt bud er, at vi lander på en middeltemperatur  omkring -0.6 °C +/- nogle 10’endedele. 
+                      Det er pænt under den nye klimanormal fra 1991-2020, der lyder på 1,5 °C for februar. Vi skal dog kun 3 år 
+                      tilbage til 2018, hvor februar endte på -0,7 °C i middeltemperatur. Siden 1991 har vi faktisk haft 7 februar-måneder 
+                      der var koldere end i år, så med mildere vejr i vente kan vi ikke kalde måneden sensationel kold.”</p>
+                      <br>
+                      <p class="fw-bold">Hele vinteren så – den må da have været kold!?</p>
+                      <br>
+                      <p>Også her har Mikael Scharling et bud på, hvor vintermånederne samlet ender.
+                      <br>
+                      ”Som det ser ud nu, er mit bud, at vi lander tæt på 1,5 °C i middeltemperatur for hele vinteren.”
+                      <br>
+                      Den nye normal (1991-2020) afslører, at vinteren i gennemsnit er 2,0 °C.
+                      <br>
+                      ”Dermed er vinteren 20/21 kun 0,5 °C koldere end normalen, og man kan sige, at vinteren ender 
+                      til den lidt kølige side, men så heller ikke mere. I normalperioden (1991-2020) har vintrene 11 
+                      gange været lige så kold eller koldere end i år,” afslutter Mikael Scharling.</p>
+                      <br><br>
+</div>
+</div>
 '
 
 ?>
