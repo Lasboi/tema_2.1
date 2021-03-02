82c3937ee93a9932a2cc2476f8fc81c9662c1e21
LasBoi <lasboi1337@gmail.com>
2021-03-02 11:20:43 +0100

    Rettet billederne på alle nyheders siden

--- include/all-news.php
+++ include/all-news.php
@@ -24,7 +24,7 @@ echo '
     <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
         <div class="card h-70">
-          <img src="images/ice.jpg" class="card-img-top" alt="Iskoldt">
+          <img src="images/ice.jpg" class="allNews-img-top" alt="Iskoldt">
           <div class="card-body">
             <h4 class="card-title text-center">Februar og vinteren 20/21</h4>
             <hr class="card-heading-line">
@@ -37,7 +37,7 @@ echo '
       </div>
       <div class="col">
         <div class="card h-70">
-          <img src="images/ocean.jpg" class="card-img-top" alt="Koldt">
+          <img src="images/ocean.jpg" class="allNews-img-top" alt="Koldt">
           <div class="card-body">
             <h4 class="card-title text-center">Hvorfor er det så koldt?</h4>
             <hr class="card-heading-line">
@@ -50,7 +50,7 @@ echo '
       </div>
       <div class="col">
         <div class="card h-70">
-          <img src="images/winter.jpg" class="card-img-top" alt="Vinter">
+          <img src="images/winter.jpg" class="allNews-img-top" alt="Vinter">
           <div class="card-body">
             <h4 class="card-title text-center">Den koldeste nat i 9 år</h4>
             <hr class="card-heading-line">
@@ -67,7 +67,7 @@ echo '
       <div class="row row-cols-1 row-cols-md-3 g-4">
       <div class="col">
         <div class="card h-70">
-          <img src="images/pollen.png" class="card-img-top" alt="Forår">
+          <img src="images/pollen.png" class="allNews-img-top" alt="Forår">
           <div class="card-body">
             <h4 class="card-title text-center">Pollen på rekordkurs</h4>
             <hr class="card-heading-line">
@@ -80,7 +80,7 @@ echo '
       </div>
       <div class="col">
         <div class="card h-70">
-          <img src="images/lake.png" class="card-img-top" alt="Iskold sø">
+          <img src="images/lake.png" class="allNews-img-top" alt="Iskold sø">
           <div class="card-body">
             <h4 class="card-title text-center">Isens bæreevne</h4>
             <hr class="card-heading-line">
@@ -93,7 +93,7 @@ echo '
       </div>
       <div class="col">
         <div class="card h-70">
-          <img src="images/sign.png" class="card-img-top" alt="Skilt">
+          <img src="images/sign.png" class="allNews-img-top" alt="Skilt">
           <div class="card-body">
             <h4 class="card-title text-center">Glatte veje</h4>
             <hr class="card-heading-line">
