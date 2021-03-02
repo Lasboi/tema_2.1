7989ccb52358b626a574b30ad3a02e4f5e772ef5
LasBoi <lasboi1337@gmail.com>
2021-03-02 12:11:17 +0100

    Rettet skrifttype på alt tekst

--- include/cities.php
+++ include/cities.php
@@ -17,7 +17,7 @@ echo '
   <div><button type="button" class="btn btn-outline-danger">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path></svg></button></div>
-  <div><div class="d-flex p-2 bd-highlight">Slagelse</div></div>
+  <div><div class="d-flex p-2 bd-highlight"><p>Slagelse</p></div></div>
   <div><img src="images/sun1.png" class="city-image" alt="Sol"></div>  
   <p class="city-text-content"> 9°/6°</p>
   <p class="city-text-content">0 mm</p>
@@ -30,7 +30,7 @@ echo '
   <div><button type="button" class="btn btn-outline-danger">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path></svg></button></div>
-  <div><div class="d-flex p-2 bd-highlight">Vemmelev</div></div>
+  <div><div class="d-flex p-2 bd-highlight"><p>Vemmelev</p></div></div>
   <div><img src="images/cloud1.png" class="city-image2" alt="sky"></div>  
   <p class="city-text-content">9°/6°</p>
   <p class="city-text-content">1 mm</p>
@@ -42,7 +42,7 @@ echo '
   <div><button type="button" class="btn btn-outline-danger">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"></path></svg></button></div>
-  <div><div class="d-flex p-2 bd-highlight">Korsør</div></div>
+  <div><div class="d-flex p-2 bd-highlight"><p>Korsør</p></div></div>
   <div><img src="images/sun1.png" class="city-image" alt="sol"></div>  
   <p class="city-text-content">9°/6°</p>
   <p class="city-text-content">0 mm</p>
